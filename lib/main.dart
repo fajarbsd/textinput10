19f7e095412708910f56cc87b2d1a4ae935066e8
fajarbsd <fajar.fakhran@apsschef.com>
2021-12-24 17:05:27 +0700

    tambah snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    home: Home(),
     title: "Text Input",
+    home: Home(),
   ));
 }
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-String? teks="";
-TextEditingController controller = TextEditingController(); //controller clear inputan setelah submit
+  String? teks = "";
+  TextEditingController controllerInput =
+      TextEditingController(); //controller clear inputan setelah submit
+  TextEditingController controllerAlert = TextEditingController();
+  TextEditingController controllerSnackbar = TextEditingController();
 
-void _alertdialog(String str){ //method menampilkan dialog
-  if(str.isEmpty) return;
+  @override
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
 
-  AlertDialog alertDialog = AlertDialog(
-    content: Text(str, style: TextStyle(fontSize: 20.0),),
-  );
+  void _snackbar(String str) {
+    if (str.isEmpty) return;
 
-  showDialog(builder: (context) => alertDialog, context: context);
-}
+    _scaffoldState.currentState!.showSnackBar(SnackBar(
+      content: Text(
+        str,
+        style: TextStyle(fontSize: 20.0),
+      ),
+      duration: Duration(seconds: 2),
+    ));
+  }
+
+  void _alertdialog(String str) {
+    //method menampilkan dialog
+    if (str.isEmpty) return;
+
+    AlertDialog alertDialog = AlertDialog(
+      content: Text(
+        str,
+        style: TextStyle(fontSize: 20.0),
+      ),
+      actions: <Widget>[
+        RaisedButton(
+            color: Colors.purple,
+            child: Text(
+              "Oke",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            })
+      ],
+    );
+
+    showDialog(builder: (context) => alertDialog, context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldState,
       appBar: AppBar(
         title: const Text("INPUT TEXT, ALERT DIALOG & SNACKBAR"),
-        backgroundColor: Colors.purple,),
-      
+        backgroundColor: Colors.purple,
+      ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: controller, //init controller clear inputan
-              decoration:const InputDecoration(
+        child: Column(children: <Widget>[
+          TextField(
+            controller: controllerInput, //init controller clear inputan
+            decoration: const InputDecoration(
               hintText: "Tulis di sini untuk teks",
-              ),
-              onSubmitted: (String str){
-                setState(() {
-                  teks=str + '\n' + teks!; //menambah teks tanpa menghilangkan str teks sebelumnya
-                  controller.text=""; //penggunaan controller clear inputan
-                });
-              },
             ),
-            Text(teks!, style: const TextStyle(fontSize: 20.0),),
-
-            TextField(
-              controller: controller, //init controller clear inputan
-              decoration:const InputDecoration(
-              hintText: "Tulis untuk alert",
-              ),
-              onSubmitted: (String str){
-                _alertdialog(str);
-              },
+            onSubmitted: (String str) {
+              setState(() {
+                teks = str +
+                    '\n' +
+                    teks!; //menambah teks tanpa menghilangkan str teks sebelumnya
+                controllerInput.text = ""; //penggunaan controller clear inputan
+              });
+            },
+          ),
+          Text(
+            teks!,
+            style: const TextStyle(fontSize: 20.0),
+          ),
+          TextField(
+            controller: controllerAlert, //init controller clear inputan
+            decoration: const InputDecoration(
+              hintText: "Tulis untuk snackbar",
             ),
-            
-            
-          ]
-        ),),
+            onSubmitted: (String str) {
+              _alertdialog(str);
+              controllerAlert.text = "";
+            },
+          ),
+          TextField(
+            controller: controllerSnackbar, //init controller clear inputan
+            decoration: const InputDecoration(
+              hintText: "Tulis untuk snackbar",
+            ),
+            onSubmitted: (String str) {
+              _snackbar(str);
+              controllerSnackbar.text = "";
+            },
+          ),
+        ]),
+      ),
     );
   }
 }
